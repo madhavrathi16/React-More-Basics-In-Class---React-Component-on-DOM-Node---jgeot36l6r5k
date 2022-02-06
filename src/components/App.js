46fd01ad8ffbcdaf5448cf4208a08f2c1c16d1d0
Madhav Rathi <93340695+madhavrathi16@users.noreply.github.com>
2022-02-06 16:44:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
 
 function App() {
   return (
-    <div className="App">
-    <p>
-      Now I can render any React component on any DOM node I want using ReactDOM.render
-    </p>
-    </div>
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
   );
 }
 
